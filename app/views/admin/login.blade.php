1360596428ac469ff4e530572a5e05cc4bf69d3f
Jefferson Raga Salunga <jefferson.salunga@yahoo.com>
2014-05-09 16:55:52 +0800

    user

--- app/views/admin/login.blade.php
+++ app/views/admin/login.blade.php
@@ -11,7 +11,7 @@ Login
 <div class="login-container img-rounded">
 	<?= '<span style="color:red">' . Session::get('login_error') . '</span>' ?>
 	<?= Form::open(array('role'=>'form')) ?>
-    <?= Form::text('email', '', array('class'=>'form-control', 'placeholder'=>'Email')) ?>
+    <?= Form::text('email', '', array('class'=>'form-control', 'placeholder'=>'Username')) ?>
     <?= Form::password('password', array('class'=>'form-control', 'placeholder'=>'Password')) ?>
     <br>
     <?= Form::submit('Log In', array('class'=>'btn btn-lg btn-primary btn-block')) ?>
